fe90c959eb2f8d1dc9107dcf6b2af52df495c881
Arvind Shelke <ucdonor5@gmail.com>
2023-01-04 22:07:20 +0530

    patterns/basic pyramid

--- patterns/basic.cpp
+++ patterns/basic.cpp
@@ -25,7 +25,7 @@ void halfReversedRightPyramid(int rows, int cols) {
 		for (int j = i; j > 0; j--) {
 			cout << "* ";
 		}
-		if(i==1) return;
+		if (i == 1) return;
 		cout << "\n";
 	}
 }
@@ -34,7 +34,7 @@ void halfReversedRightPyramid(int rows, int cols) {
 void halfRightPyramidWithNums(int rows, int cols) {
 	for (int i = 0; i < rows; i++) {
 		for (int j = 0; j <= i; j++) {
-			cout << j+1 << " ";
+			cout << j + 1 << " ";
 		}
 		cout << "\n";
 	}
@@ -43,17 +43,48 @@ void halfRightPyramidWithNums(int rows, int cols) {
 void halfRightPyramidWithSameNumsInRows(int rows, int cols) {
 	for (int i = 0; i < rows; i++) {
 		for (int j = 0; j <= i; j++) {
-			cout << i+1 << " ";
+			cout << i + 1 << " ";
 		}
 		cout << "\n";
 	}
 }
 
+void pyramid(int rows, int cols) {
+	for (int i = 0; i < rows; i++) {
+		const int COL = 2 * cols + 1;
+		// cout << COL;
+
+		// space star space
+		//  4 1 4
+		//  3 3 3
+		//  2 5 2
+		//  1 7 1
+		//  0 9 0
+
+		// print spaces
+		for (int j = rows - i - 1; j > 0; j--) {
+			cout << "  ";
+		}
+
+		// print stars
+		for (int j = 0; j < 2 * i + 1; j++) {
+			cout << "* ";
+		}
+
+		// print spaces again
+		for (int j = rows - i - 1; j > 0; j--) {
+			cout << "  ";
+		}
+		if (i == rows - 1) return;
+		cout << "\n";
+	}
+}
+
 int main()
-{	
+{
 	int rows, cols;
-	cin>>rows;
-	cin>>cols;
-	halfReversedRightPyramid(rows, cols);
+	cin >> rows;
+	cin >> cols;
+	pyramid(rows, cols);
 	return 0;
 }
